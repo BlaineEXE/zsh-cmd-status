88047b0751e1d5eac3a10b3d43108364cb547528
Blaine Gardner <blaine.gardner@redhat.com>
2021-02-01 20:23:47 -0700

    Initial version of plugin with doc
    Signed-off-by: Blaine Gardner <blaine.gardner@redhat.com>

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # zsh-cmd-status
-ZSH plugin to report the status of commands including return code and time taken.
+ZSH plugin to report the status of commands including return code and time taken (duration).
+
+This plugin will report the return code of the previously run command if it returns nonzero.
+It will report the time taken to run the command if its greater than the duration threshold
+([ZSH_CMD_STATUS_DURATION_THRESHOLD](#config)).
+
+## Examples
+
+```
+> false
+
+returned 1
+```
+
+```
+> sleep 10
+
+took 10s
+```
+
+```
+> sleep 11 && false
+
+returned 1 & took 11s
+```
+
+```
+> grep
+usage: grep [-abcDEFGHhIiJLlmnOoqRSsUVvwxZ] [-A num] [-B num] [-C[num]]
+	[-e pattern] [-f file] [--binary-files=value] [--color=when]
+	[--context[=num]] [--directories=action] [--label] [--line-buffered]
+	[--null] [pattern] [file ...]
+
+returned 2
+```
+
+
+
+## Config
+| Option env var                    | Default | Description                                                 |
+| --------------------------------- | ------- | ----------------------------------------------------------- |
+| ZSH_CMD_STATUS_DURATION_THRESHOLD | 10      | time in seconds under which duration should not be reported |
---
+++ cmd-status.plugin.zsh
@@ -0,0 +1,65 @@
+
+# load colors module that allows things like $fg[red]
+autoload colors && colors
+
+: ${ZSH_CMD_STATUS_DURATION_THRESHOLD:=10}
+
+function _zsh_cmd_return_code() {
+  local code=$?
+  if [[ code -ne 0 ]]; then
+    printf "${fg_bold[red]}returned ${code}${reset_color}"
+  fi
+}
+
+function _zsh_cmd_render_duration() {
+  local duration=$1
+  local sec=$(( duration % 60 ))
+  local min=$(( duration % 3600 / 60 ))
+  local hour=$(( duration / 3600 ))
+  if [[ $hour -gt 0 ]]; then
+    printf '%dh %dm %ds' $hour $min $sec
+  elif [[ $min -gt 0 ]]; then
+    printf '%dm %ds' $min $sec
+  else
+    printf '%ds' $sec
+  fi
+}
+
+function _zsh_cmd_duration() {
+  if [ -z "${zsh_cmd_status_start_time}" ]; then
+      return 0
+  fi
+  local end_time
+  end_time="$( date +%s )"
+  local duration=$(( end_time - zsh_cmd_status_start_time ))
+  local threshold=${ZSH_CMD_STATUS_DURATION_THRESHOLD:=10}
+  if [ "$duration" -lt "$threshold" ]; then
+    return 0
+  fi
+  pretty="$(_zsh_cmd_render_duration ${duration})"
+  printf "${fg_bold[yellow]}took ${pretty}${reset_color}"
+}
+
+function _zsh-cmd-status-preexec() {
+  zsh_cmd_status_start_time="$( date +%s )"
+}
+
+function _zsh-cmd-status-precmd() {
+  local ret="$(_zsh_cmd_return_code)"
+  local dur="$(_zsh_cmd_duration)"
+
+  # Join the retcode and duration with an ampersand
+  # If one is unset, no ampersand will be added
+  # if both are unset, nothing will be added
+  local out=(${ret} ${dur})
+  out="${(j: & :)out}"
+  if [[ -n "${out}" ]]; then
+    printf "\n${out}\n"
+  fi
+
+  unset _start_time
+}
+
+autoload -Uz add-zsh-hook
+add-zsh-hook preexec _zsh-cmd-status-preexec
+add-zsh-hook precmd _zsh-cmd-status-precmd
